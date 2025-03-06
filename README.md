61b65c99b607d704c72aa974ebefa2fc9363e931
denhampreen <denhampreen@gmail.com>
2025-03-06 11:47:47 +0200

    feat: user & global stats

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-## Envio Blank Template
+# Dusted Envio indexer
 
 *Please refer to the [documentation website](https://docs.envio.dev) for a thorough guide on all [Envio](https://envio.dev) indexer features*
+
+## Run
+
+```bash
+pnpm envio dev
+```
+
+## Pre-requisites
+
+- [Node.js (use v18 or newer)](https://nodejs.org/en/download/current)
+- [pnpm (use v8 or newer)](https://pnpm.io/installation)
+- [Docker desktop](https://www.docker.com/products/docker-desktop/)
+
+## Example queries
+
+Visit http://localhost:8080 to see the GraphQL Playground, local password is `testing`.
+
+### Global data
+
+```graphql
+query MyQuery {
+  GlobalStats(where: {id: {_eq: "global"}}) {
+    tippingFee
+    tippingOwner
+    pointsOwner
+    pointsClaimable
+  }
+}
+```
+
+### User data
+
+```graphql
+query MyQuery {
+  User {
+    address
+    id
+    points {
+      totalPointsClaimed
+    }
+    tips {
+      amount
+      sender
+    }
+  }
+}
+```
--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -13,15 +13,57 @@ import {
   Tipping_TipSent,
 } from "generated";
 
+const createOrUpdateGlobalStats = async (fields: any, context: any) => {
+  let globalStats = await context.GlobalStats.get("global");
+
+  if (globalStats) {
+    // Update only the fields that exist in the event object
+    for (const key in fields) {
+      if (fields[key] !== undefined && key in globalStats) {
+        globalStats[key] = fields[key];
+      }
+    }
+  } else {
+    globalStats = {
+      id: "global",
+      tippingFee: BigInt(0),
+      tippingOwner: "deployer address",
+      pointsOwner: "deployer address",
+      pointsClaimable: false
+    };
+  }
+
+  await context.GlobalStats.set(globalStats);
+
+};
+
+const createUser = async (address: string, context: any) => {
+  let user = await context.User.get(address)
+
+  if (!user) {
+     user = {
+       id: address,
+       address: address,      
+     }
+     context.User.set(user);
+  } 
+} 
+
 PointsRedeemer.Claimed.handler(async ({ event, context }) => {
+
+  const {user, tokenAmount, totalPointsClaimed} = event.params;
+
   const entity: PointsRedeemer_Claimed = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    user: event.params.user,
-    tokenAmount: event.params.tokenAmount,
-    totalPointsClaimed: event.params.totalPointsClaimed,
+    user: user,
+    tokenAmount: tokenAmount,
+    totalPointsClaimed: totalPointsClaimed,
   };
 
   context.PointsRedeemer_Claimed.set(entity);
+
+ await createUser(user, context);
+
 });
 
 PointsRedeemer.ClaimsToggled.handler(async ({ event, context }) => {
@@ -31,6 +73,8 @@ PointsRedeemer.ClaimsToggled.handler(async ({ event, context }) => {
   };
 
   context.PointsRedeemer_ClaimsToggled.set(entity);
+
+  await createOrUpdateGlobalStats({pointsClaimable: event.params.enabled}, context);
 });
 
 PointsRedeemer.OwnershipTransferred.handler(async ({ event, context }) => {
@@ -41,6 +85,8 @@ PointsRedeemer.OwnershipTransferred.handler(async ({ event, context }) => {
   };
 
   context.PointsRedeemer_OwnershipTransferred.set(entity);
+
+  await createOrUpdateGlobalStats({pointsOwner: event.params.newOwner}, context);
 });
 
 PointsRedeemer.Withdraw.handler(async ({ event, context }) => {
@@ -59,6 +105,8 @@ Tipping.FeeUpdated.handler(async ({ event, context }) => {
   };
 
   context.Tipping_FeeUpdated.set(entity);
+
+  await createOrUpdateGlobalStats({tippingFee: event.params.newFee}, context);
 });
 
 Tipping.OwnershipTransferred.handler(async ({ event, context }) => {
@@ -69,15 +117,22 @@ Tipping.OwnershipTransferred.handler(async ({ event, context }) => {
   };
 
   context.Tipping_OwnershipTransferred.set(entity);
+
+  await createOrUpdateGlobalStats({tippingOwner: event.params.newOwner}, context);
 });
 
 Tipping.TipSent.handler(async ({ event, context }) => {
+
+  let {sender, recipient, amount} = event.params;
+
   const entity: Tipping_TipSent = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    sender: event.params.sender,
-    recipient: event.params.recipient,
-    amount: event.params.amount,
+    sender: sender,
+    recipient: recipient,
+    amount: amount,
   };
 
   context.Tipping_TipSent.set(entity);
+
+  await createUser(sender, context);
 });
